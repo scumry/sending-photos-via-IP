fa468a75e8a2ac264626523ac7da6c7f1ffbc0f1
scumry <diana.ma1310@mail.ru>
2023-12-05 22:12:12 +0500

    done

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+//var tcpListener = new TcpListener(IPAddress.Any, 8888);
+try
+{
+    // Подключение к серверу
+    await tcpClient.ConnectAsync("192.168.220.139", 1777);
+    while (true)
+    {
+
+    // чтение изображения ввиде массива
+    byte[] imageData = File.ReadAllBytes("img/kot.jpg");
+
+    //Создание сообщения
+    //System.Console.WriteLine("Введите команду для сервера");
+    //string command = Console.ReadLine() + '\n';
+
+    //Перeвод запроса из строкового типа данных в массив байт
+    byte[] requestData = File.ReadAllBytes("img/kot.jpg");
+
+    // Отправка сообщения серверу
+    await tcpClient.SendAsync(requestData, SocketFlags.None);
+
+    // Оповещение о состоянии сообщения
+    Console.WriteLine("Изображение отправленно");
+
+    // Сообщение буфера для приема ответа от сервера
+    byte[] data = new byte[512];
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
